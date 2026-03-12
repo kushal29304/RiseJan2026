0b8eafa9b9a94d71499c6582ce8f82f25df84a14
Kushal Parikh <121802561+kushal29304@users.noreply.github.com>
2026-03-12 18:05:02 +0530

    Add files via upload

---
+++ Java/src/FullStackJava/Presentation.java
@@ -0,0 +1,92 @@
+package FullStackJava;
+
+public class Presentation {
+    public static void main(String[] args){
+//        if else
+//        int k= 18;
+//        if( k>18){
+//            System.out.println("Eligible for voting!");
+//        }
+//        else {
+//            System.out.println("Not eligible for voting!");
+//        }
+
+//        //else if example
+//        int marks = 75;
+//
+//        if(marks >= 90){
+//            System.out.println("Grade A");
+//        }
+//        else if(marks >= 70){
+//            System.out.println("Grade B");
+//        }
+//        else if(marks >= 50){
+//            System.out.println("Grade C");
+//        }
+//        else{
+//            System.out.println("Fail");
+//        }
+
+//        Nested if
+//        String a= "Indian";
+//        int age=19;
+//        if(a == "Indian"){
+//            System.out.println("The person is the citizen of India");
+//            if ( age>18){
+//                System.out.println("Eligible to vote");
+//            }
+//            else{
+//                System.out.println("Not eligible to vote");
+//            }
+//        }
+//        else {
+//            System.out.println("You are not a citizen of India so you cannot vote");
+//        }
+
+        //Switch example
+        int day = 2;
+
+        switch(day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+
+
+//        for loop
+//        for(int i = 1; i <= 5; i++) {
+//            System.out.println("Number: " + i);
+//        }
+
+//        while loop
+//        int i = 1;   // starting value
+//
+//        while(i <= 5) {   // condition is checked before running the loop
+//            System.out.println("Number: " + i);
+//            i++;   // increase the value of i by 1
+//        }
+
+//        do-while loop
+//        int i = 1;   // starting value
+//
+//        do {
+//            System.out.println("Number: " + i);4
+//            i++;   // increase the value after each iteration
+//        }
+//        while(i <= 5);   // condition checked after execution
+
+    }
+
+}
+
